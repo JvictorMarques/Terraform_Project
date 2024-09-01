87ef448c98209b753115ec6086b0dcc51c5f7739
JvictorMarques <jvictorsilvamarques358@gmail.com>
2024-09-01 19:48:47 -0300

    first commit

---
+++ modules/s3/outputs.tf
@@ -0,0 +1,10 @@
+output "bucket_domain_name" {
+  value       = ""
+  sensitive   = false
+  description = "Nome do domínio do bucket"
+}
+output "bucket_region" {
+  value       = ""
+  sensitive   = false
+  description = "Região do bucket"
+}
