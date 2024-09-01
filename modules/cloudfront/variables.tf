f8a14864a60deca6b5b1a9c32cfea259a10ac51b
JvictorMarques <jvictorsilvamarques358@gmail.com>
2024-09-01 20:34:35 -0300

    feature/5: "Creating S3 Bucket and CloudFront"

---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,8 @@
+variable origin_id {
+  type        = string
+  description = "ID S3"
+}
+variable bucket_domain_name {
+  type        = string
+  description = "Domínio S3"
+}
--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,10 +1,11 @@
 output "bucket_domain_name" {
-  value       = ""
+  value       = aws_s3_bucket.bucket.bucket_domain_name
   sensitive   = false
   description = "Nome do domínio do bucket"
 }
-output "bucket_region" {
-  value       = ""
+
+output "bucket_id" {
+  value       = aws_s3_bucket.bucket.id
   sensitive   = false
-  description = "Região do bucket"
+  description = "ID de domínio do bucket"
 }
